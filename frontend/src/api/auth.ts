734a4eecc7e98cf80f6bcf5cbcd5713c56fd8c14
Saloquin <saloquin24@gmail.com>
2026-01-13 01:23:53 +0100

    mock auth

---
+++ frontend/src/api/auth.ts
@@ -0,0 +1,17 @@
+import type { User } from "../model/db/user";
+import type { Login } from "../model/domain/login";
+
+
+
+export const apiLogin = async (login: Login): Promise<{ token: string; user: User }> => {
+    return {
+        token: "fake-jwt-token-123456",
+        user: {
+            name: "Magnin",
+            first_name: "Christelle",
+            email: login.email,
+            role: "admin",
+            id: 1,
+        }
+    };
+}
