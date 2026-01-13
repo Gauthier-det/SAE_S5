2c6ca2555714197c5b968515e6065d3106d3eb64
Raphaël <akraponix@gmail.com>
2026-01-13 15:22:07 +0100

    Added a basic UserController

---
+++ laravel/app/Http/Controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function getAllUsers()
+    {
+        $users = User::all();
+
+        return response()->json(['data' => $users]);
+    }
+
+    public function getUserById($id)
+    {
+        $user = User::find($id);
+
+        if (! $user) {
+            return response()->json([
+                'message' => 'User not found',
+            ], 404);
+        }
+
+        return response()->json(['data' => $user], 200);
+    }
+}
