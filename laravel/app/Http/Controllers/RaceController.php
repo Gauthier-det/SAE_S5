f9af29255a0fca71151fa4a6d12369263105fdd1
Drygnos <brault232@unicaen.fr>
2026-01-13 11:35:23 +0100

    fix: createRace uses a validator

--- laravel/app/Http/Controllers/RaceController.php
+++ laravel/app/Http/Controllers/RaceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\Race;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class RaceController extends Controller
 {
@@ -24,7 +25,44 @@ class RaceController extends Controller
 
     public function createRace(Request $request)
     {
-        $race = Race::create($request->all());
+        $validator = Validator::make($request->all(), [
+            'USE_ID' => 'required|integer|exists:SAN_USERS,USE_ID',
+            'RAI_ID' => 'required|integer|exists:SAN_RAIDS,RAI_ID',
+            'RAC_TIME_START' => 'required|date',
+            'RAC_TIME_END' => 'required|date|after_or_equal:RAC_TIME_START',
+            'RAC_TYPE' => 'required|string|max:255',
+            'RAC_DIFFICULTY' => 'required|string|max:255',
+            'RAC_MIN_PARTICIPANTS' => 'required|integer|min:0',
+            'RAC_MAX_PARTICIPANTS' => 'required|integer|min:0|gte:RAC_MIN_PARTICIPANTS',
+            'RAC_MIN_TEAMS' => 'required|integer|min:0',
+            'RAC_MAX_TEAMS' => 'required|integer|min:0|gte:RAC_MIN_TEAMS',
+            'RAC_TEAM_MEMBERS' => 'required|integer|min:0',
+            'RAC_AGE_MIN' => 'required|integer|min:0',
+            'RAC_AGE_MIDDLE' => 'required|integer|min:0',
+            'RAC_AGE_MAX' => 'required|integer|min:0|gte:RAC_AGE_MIDDLE',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $race = Race::create($request->only([
+            'USE_ID',
+            'RAI_ID',
+            'RAC_TIME_START',
+            'RAC_TIME_END',
+            'RAC_TYPE',
+            'RAC_DIFFICULTY',
+            'RAC_MIN_PARTICIPANTS',
+            'RAC_MAX_PARTICIPANTS',
+            'RAC_MIN_TEAMS',
+            'RAC_MAX_TEAMS',
+            'RAC_TEAM_MEMBERS',
+            'RAC_AGE_MIN',
+            'RAC_AGE_MIDDLE',
+            'RAC_AGE_MAX',
+        ]));
+
         return response()->json(['data' => $race], 201);
     }
 }
