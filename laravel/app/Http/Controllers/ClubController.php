c42aba6205d05c4c0db7392fc4c7d3b8cd5c00f9
Drygnos <brault232@unicaen.fr>
2026-01-14 14:44:18 +0100

    feat: create club with address

--- laravel/app/Http/Controllers/ClubController.php
+++ laravel/app/Http/Controllers/ClubController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Club;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 
 class ClubController extends Controller
 {
@@ -44,6 +45,48 @@ class ClubController extends Controller
         ]));
 
         return response()->json(['data' => $club], 201);
+    }
+
+    public function createClubWithAddress(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'USE_ID' => 'required|integer|exists:SAN_USERS,USE_ID',
+            'CLU_NAME' => 'required|string|max:255',
+            'ADD_POSTAL_CODE' => 'required|integer|digits:5',
+            'ADD_CITY' => 'required|string|max:255',
+            'ADD_STREET_NAME' => 'required|string|max:255',
+            'ADD_STREET_NUMBER' => 'required|string|max:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        try {
+            DB::beginTransaction();
+
+            // Create the address
+            $addressId = DB::table('SAN_ADDRESSES')->insertGetId([
+                'ADD_POSTAL_CODE' => $request->input('ADD_POSTAL_CODE'),
+                'ADD_CITY' => $request->input('ADD_CITY'),
+                'ADD_STREET_NAME' => $request->input('ADD_STREET_NAME'),
+                'ADD_STREET_NUMBER' => $request->input('ADD_STREET_NUMBER'),
+            ]);
+
+            // Create the club with the address
+            $club = Club::create([
+                'USE_ID' => $request->input('USE_ID'),
+                'ADD_ID' => $addressId,
+                'CLU_NAME' => $request->input('CLU_NAME'),
+            ]);
+
+            DB::commit();
+
+            return response()->json(['data' => $club], 201);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json(['message' => 'Error creating club with address', 'error' => $e->getMessage()], 500);
+        }
     }
 
     public function updateClub(Request $request, $id)
